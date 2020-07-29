1d042f7f697eb940eeba002e3204bd8093fd6433
Andrew Ribas <andrew.j.ribas@gmail.com>
2020-07-29 17:03:24 +0000

    first commit

---
+++ lib/pokemoves/cli.rb
@@ -0,0 +1,8 @@
+require "pokemoves/cli/version"
+
+module Pokemoves
+  module Cli
+    class Error < StandardError; end
+    # Your code goes here...
+  end
+end
---
+++ lib/pokemoves/cli/version.rb
@@ -0,0 +1,5 @@
+module Pokemoves
+  module Cli
+    VERSION = "0.1.0"
+  end
+end
