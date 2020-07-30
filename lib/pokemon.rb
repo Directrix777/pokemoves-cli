a9fb17411f2381082a605c488d7903b42a24f70b
Andrew Ribas <andrew.j.ribas@gmail.com>
2020-07-30 21:33:47 +0000

    Added class method find_or_create_by_name for Pokemon

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -7,9 +7,19 @@ class Pokemon
   def initialize(name)
     @name = name
     @type = Scraper.get_pokemon_by_name(name)["types"][0]["type"]["name"]
+    @@all << self
+    binding.pry
   end
   
+  def self.all
+    @@all
+  end
   
+  def self.find_or_create_by_name(name)
+    if Scraper.pokemons.include?(name)
+      return self.all.detect{|a| a.name == name} if self.all.collect{|a| a.name}.include?(name)
+      return Pokemon.new(name)
+    end
+    return nil
+  end
 end
-
-puts Pokemon.new("sylveon").type
