2944079275c8792d28045b93ad57bd23d984f0de
Andrew Ribas <andrew.j.ribas@gmail.com>
2020-07-30 18:11:16 +0000

    added class method get_move_by_name

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -5,12 +5,20 @@ class Scraper
   
   url = "https://pokeapi.co/api/v2/"
   pokemons = HTTParty.get(url + "/pokemon?offset=0&limit=964").parsed_response
-  moves = HTTParty.get(url + "move?offset=0&limit=746").parsed_response
+  moves = HTTParty.get(url + "/move?offset=0&limit=746").parsed_response
   
   def self.get_pokemon_by_name(name)
     pokemons.each{|pokemon|
       if pokemon["name"] == name
         return HTTParty.get(pokemon["url"])
+      end
+    }
+  end
+  
+  def self.get_move_by_name(name)
+    moves.each{|move|
+      if move["name"] == name
+        return HTTParty.get(move["url"])
       end
     }
   end
