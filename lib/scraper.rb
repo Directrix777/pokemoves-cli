800aa50a447b1430726854f9e51435710fd68157
Andrew Ribas <andrew.j.ribas@gmail.com>
2020-07-30 18:04:01 +0000

    added method to get pokemon by name

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -4,13 +4,23 @@ require "pry"
 class Scraper
   
   url = "https://pokeapi.co/api/v2/"
-  pokemon = []
+  @@pokemon = []
   
   def self.scrape_pokemon()
     pokemon_url_list = HTTParty.get(url + "/pokemon?offset=0&limit=964")
     pokemon_url_list.parsed_response
-    pokemon = pokemon_url_list
+    @@pokemon = pokemon_url_list
   end
+  
+  def self.get_pokemon_by_name(name)
+    @@pokemon.each{|pokemon|
+      if pokemon["name"] == name
+        return HTTParty.get(pokemon["url"])
+      end
+    }
+  end
+  
+  
   
 end
 
