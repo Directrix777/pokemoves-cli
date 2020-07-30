77bfb08a07d841ef09dc973e008145d53f391e78
Andrew Ribas <andrew.j.ribas@gmail.com>
2020-07-30 18:09:01 +0000

    removed scrape pokemon method, made scraping part of the class

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -4,16 +4,11 @@ require "pry"
 class Scraper
   
   url = "https://pokeapi.co/api/v2/"
-  @@pokemon = []
-  
-  def self.scrape_pokemon()
-    pokemon_url_list = HTTParty.get(url + "/pokemon?offset=0&limit=964")
-    pokemon_url_list.parsed_response
-    @@pokemon = pokemon_url_list
-  end
+  pokemons = HTTParty.get(url + "/pokemon?offset=0&limit=964").parsed_response
+  moves = HTTParty.get(url + "move?offset=0&limit=746").parsed_response
   
   def self.get_pokemon_by_name(name)
-    @@pokemon.each{|pokemon|
+    pokemons.each{|pokemon|
       if pokemon["name"] == name
         return HTTParty.get(pokemon["url"])
       end
