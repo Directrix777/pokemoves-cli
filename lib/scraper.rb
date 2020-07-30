3a5b6199324a07c68e8f0c98fc924b4093b7f917
Andrew Ribas <andrew.j.ribas@gmail.com>
2020-07-30 17:18:15 +0000

    Added a second scrape to scrape each pokemon from the list of pokemon

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -3,10 +3,22 @@ require "pry"
 
 class Scraper
   
+  attr_reader :url
+  
   def initialize
-    response = HTTParty.get("https://pokeapi.co/api/v2/")
+    @url = "https://pokeapi.co/api/v2/"
+  end
+  
+  def scrape_pokemon()
+    pokemon_url_list = HTTParty.get(url + "/pokemon?offset=0&limit=964")
+    pokemon_url_list.parsed_response
+    pokemon_list = []
+    pokemon_url_list["results"].each{|pokemon_url|
+      pokemon_list << HTTParty.get(pokemon_url[url]).parsed_response
+    }
     binding.pry
   end
+  
 end
 
-Scraper.new
+Scraper.new.scrape_pokemon
