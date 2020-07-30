509fa81bf19c20731b3eb00cdecbb9c54f072165
Andrew Ribas <andrew.j.ribas@gmail.com>
2020-07-30 18:18:07 +0000

    fixed error with scraping. Now class variables are initialized correctly

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -4,11 +4,12 @@ require "pry"
 class Scraper
   
   url = "https://pokeapi.co/api/v2/"
-  pokemons = HTTParty.get(url + "/pokemon?offset=0&limit=964").parsed_response
-  moves = HTTParty.get(url + "/move?offset=0&limit=746").parsed_response
+  @@pokemons = HTTParty.get(url + "/pokemon?offset=0&limit=964").parsed_response["results"]
+  @@moves = HTTParty.get(url + "/move?offset=0&limit=746").parsed_response["results"]
   
   def self.get_pokemon_by_name(name)
-    pokemons.each{|pokemon|
+    binding.pry
+    @@pokemons.each{|pokemon|
       if pokemon["name"] == name
         return HTTParty.get(pokemon["url"])
       end
@@ -16,7 +17,7 @@ class Scraper
   end
   
   def self.get_move_by_name(name)
-    moves.each{|move|
+    @@moves.each{|move|
       if move["name"] == name
         return HTTParty.get(move["url"])
       end
@@ -27,4 +28,4 @@ class Scraper
   
 end
 
-Scraper.scrape_pokemon
+puts Scraper.get_move_by_name("cut")["name"]
